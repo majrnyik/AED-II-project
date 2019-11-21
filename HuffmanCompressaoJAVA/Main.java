807d234b94854c4dda0f850b162a342f6e83f324
Pedro Henrique Lopes <56567741+pedro-hbl@users.noreply.github.com>
2019-11-21 14:49:50 -0300

    Update Main.java

--- HuffmanCompressaoJAVA/Main.java
+++ HuffmanCompressaoJAVA/Main.java
@@ -1,4 +1,30 @@
+import java.io.RandomAccessFile;
 
 public class Main {
+  public nodoLista novonodoLista(nodoArvore nArv){
+     nodoLista novo = new nodoLista();
+     if novo==NULL return NULL;
+     novo.n=nArv;
+     nodo.proximo=NULL;
+     return novo;
+  }
+  // Função que faz alocação de memória e trata os ponteiros soltos acerca de nós da árvore  
+  public nodoArvore novonodoArvore(byte c, int frequencia, nodoArvore.esquerda, nodoArvore.direita){
+    nodoArvore novo = new nodoArvore();
+    if novo==NULL return NULL;
+    //senao atribui os valorers
+    novo.c = c;
+    novo.frequencia = frequencia;
+    novo.esquerda = esquerda;
+    novo.direita = direita;
+
+    return novo;
+}
+  public static void getByteFrequency(RandomAccessFile entrada, int[] listaBytes_U) {
+    byte c;
+    
+	}
+
+  
 
 }
